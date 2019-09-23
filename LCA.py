f9dd2658a9b88040c39117004ffd32f2916709fd
henrym2 <33910429+henrym2@users.noreply.github.com>
2019-09-23 13:24:22 +0100

    Removed debugging code

--- LCA.py
+++ LCA.py
@@ -1,16 +1,6 @@
 # Lowest common ancestor - Matthew Henry
 from binarytree import tree, bst, Node
 
-# Generate a random binary tree
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
-
-print(root)
-
-
 def findPath(root, path, k):
     if root is None:
         return False
@@ -44,17 +34,3 @@ def findLCA(root, n1, n2):
             break
         i += 1
     return path1[i-1]
-
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
-
-print(root)
-
-
-print("LCA(4, 5) = %d" %(findLCA(root, 4, 5,))) 
-print("LCA(4, 6) = %d" %(findLCA(root, 4, 6)))
-print("LCA(3, 4) = %d" %(findLCA(root,3,4))) 
-print("LCA(2, 4) = %d" %(findLCA(root,2, 4)))
